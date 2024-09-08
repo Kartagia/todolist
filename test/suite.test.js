bb71ed303a2c17815c420defc277a81791e44180
Antti Kautiainen <antti@kautiainen.com>
2024-09-08 13:03:13 +0300

    test: base test suite for testing the todolist.
    The test suite test kit for testing the sub modules allowing
    to use the same tests for both webapp tests and the nodejs tests.
    
    The nodejs tests are put into "test/" folder and subfolders.
    
    Changes to be committed:
    - 	new file:   test/suite.test.js
      - test: test suite to allow calling test modules shared by the
      webapp testing and node testing.

---
+++ test/suite.test.js
@@ -0,0 +1,12 @@
+
+/**
+ * The test suite for testing whole module.
+ */
+import chai from 'chai';
+import mocha from 'mocha';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+
+describe("Node Todo List test suite", function() {
+    // Add submodule tests here with describe or it.
+});
