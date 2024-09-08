c7ece5faa75c7bb5d6483b75236d19e2c3e72c58
Antti Kautiainen <antti@kautiainen.com>
2024-09-08 14:01:28 +0300

    rename: suite test to the ES6 test module.
    Renamed the suite test to the ES6 module.
    
    Changes to be committed:
    - 	renamed:    test/suite.test.js -> test/suite.test.mjs
      - ES6 module suffix for the test suite.

--- test/suite.test.mjs
+++ test/suite.test.mjs
@@ -2,10 +2,10 @@
 /**
  * The test suite for testing whole module.
  */
-import chai from 'chai';
+import {expect, use} from 'chai';
 import mocha from 'mocha';
 import chaiAsPromised from 'chai-as-promised';
-chai.use(chaiAsPromised);
+use(chaiAsPromised);
 
 describe("Node Todo List test suite", function() {
     // Add submodule tests here with describe or it.
