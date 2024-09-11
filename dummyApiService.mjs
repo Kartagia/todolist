f094005537758cb64d7d85957ff110200b0ea9e9
Antti Kautiainen <antti@kautiainen.com>
2024-09-11 22:16:28 +0300

    feat: dummy TODO Api service
    The in memory dummy service for testing purposes.
    
    Changes to be committed:
    - 	new file:   dummyApiService.mjs
      - feat: module dummyAPiService.mjs for a dummy in memory API service
      with test purpose users.

---
+++ dummyApiService.mjs
@@ -0,0 +1,15 @@
+
+import ApiService from "./apiServices.mjs";
+/**
+ * @module dummyApiService
+ * The dummy api service for testing purposes.
+ */
+
+/**
+ * The todo api service.
+ * 
+ * @type {ApiService<import("./apiServices.mjs").Todo>}
+ */
+var service = new ApiService();
+
+export default service;
