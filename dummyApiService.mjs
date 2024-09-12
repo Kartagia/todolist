f2e6277a1dea24b4d0f9582ee2487004a5fd7cf7
Antti Kautiainen <antti@kautiainen.com>
2024-09-12 10:19:25 +0300

    feat: dummy API service for testing.
    The
    Changes to be committed:
    - 	modified:   dummyApiService.mjs
      - refactor: Implementation using InMemoryApiService

--- dummyApiService.mjs
+++ dummyApiService.mjs
@@ -1,15 +1,46 @@
 
-import ApiService from "./apiServices.mjs";
+import {InMemoryApiService} from "./apiServices.mjs";
 /**
  * @module dummyApiService
  * The dummy api service for testing purposes.
  */
 
 /**
+ * The api service properties.
+ * @type {import("./apiServices.mjs").ApiServiceParams<Todo>} 
+ */
+const apiServiceProps = {
+    /**
+     * The mapping from user identifiers to the todos of the user.
+     * @type {Record<string, TodoData>}
+     */
+    content: {},
+    /**
+     * The users of the api.
+     * @type {Record<string, UserData>}
+     */
+    users: {},
+    /**
+     * The active sessions.
+     * @type {Record<string, SessionData>}
+     */
+    sessions: {},
+    hashOptions: {
+        rounds: 200000,
+        length: 64,
+        algorithm: "sha512",
+        method: "pbkdf2"
+    },
+    sessionTimeout: 30*60*1000
+};
+
+
+
+/**
  * The todo api service.
  * 
- * @type {ApiService<import("./apiServices.mjs").Todo>}
+ * @type {import("./apiServices.mjs").ApiService<import("./apiServices.mjs").Todo>}
  */
-var service = new ApiService();
+var service = new InMemoryApiService(apiServiceProps);
 
 export default service;
