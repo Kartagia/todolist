f730caf9ad2ced6509df2dc5ad1a9684e40e5c14
Antti Kautiainen <antti@kautiainen.com>
2024-09-12 17:40:18 +0300

    feat:
    Changes to be committed:
    - 	modified:   dummyApiService.mjs
      - feat: interface EmailUser for email.

--- dummyApiService.mjs
+++ dummyApiService.mjs
@@ -6,23 +6,33 @@ import {InMemoryApiService} from "./apiServices.mjs";
  */
 
 /**
+ * The email user properties.
+ * @typedef {Object} EmailUser
+ * @property {Readonly<boolean>} [validatedEmail=false] Has the user validated email.
+ * @property {Readonly<EmailUser[]>} [backupAddresses=[]] The backup addresses of the user.
+ */
+
+/**
  * The api service properties.
- * @type {import("./apiServices.mjs").ApiServiceParams<Todo>} 
+ * @type {import("./apiServices.mjs").ApiServiceParams<Todo, void, EmailUserProperties>} 
  */
 const apiServiceProps = {
     /**
      * The mapping from user identifiers to the todos of the user.
-     * @type {Record<string, TodoData>}
+     * @type {Record<string, import("./apiServices.mjs").TodoData>}
      */
     content: {},
+
     /**
      * The users of the api.
-     * @type {Record<string, UserData>}
+     * THe mapping from user email to the user data.
+     * @type {Record<string, import("./apiServices.mjs").UserData<UserInfo>>}
      */
-    users: {},
+    users: {
+    },
     /**
      * The active sessions.
-     * @type {Record<string, SessionData>}
+     * @type {Record<string, import("./apiServices.mjs").SessionData>}
      */
     sessions: {},
     hashOptions: {
@@ -34,13 +44,35 @@ const apiServiceProps = {
     sessionTimeout: 30*60*1000
 };
 
-
-
 /**
  * The todo api service.
  * 
  * @type {import("./apiServices.mjs").ApiService<import("./apiServices.mjs").Todo>}
  */
 var service = new InMemoryApiService(apiServiceProps);
+
+(async () => {
+    const id = crypto.randomUUID();
+    let userName = "antti@kautiainen.com";
+    let secret = "F00baryMaru!";
+    console.group("Creating dummy users");
+    service.createUser(
+        userName, 
+        secret, 
+        {
+            displayName: "Administrator",
+            image: "https://www.svgrepo.com/show/70079/administrator.svg"
+        }
+    ).then(
+        (userInfo) => {
+            console.log(`Created user ${displayName} as user ${userName}[${userInfo.id}]`)
+        },
+        (error) => {
+            console.error(`The password validation failed for ${secret} due ${error.name} ${error.message}`);
+            throw error;
+        }
+    );
+    console.groupEnd();
+})();
 
 export default service;
