e32118650d76fbd69ce39ebca609b03c4a660d1a
Antti Kautiainen <antti@kautiainen.com>
2024-09-14 09:45:43 +0300

    test: apiServices construction
    Changes to be committed:
    - 	modified:   test/apiService.test.mjs
      - test: ApiServices construction

--- test/apiService.test.mjs
+++ test/apiService.test.mjs
@@ -5,15 +5,15 @@
  */
 
 import { expect } from "chai";
-import { HAS_DIGIT_REGEX, HAS_LOWER_CASE_LETTER_REGEX, HAS_PUNCTUATION_REGEX, HAS_UPPER_CASE_LETTER_REGEX, VALID_PASSWORD_REGEX } from "../apiServices.mjs";
+import createApiService, { HAS_DIGIT_REGEX, HAS_LOWER_CASE_LETTER_REGEX, HAS_PUNCTUATION_REGEX, HAS_UPPER_CASE_LETTER_REGEX, InMemoryApiService, VALID_PASSWORD_REGEX } from "../apiServices.mjs";
 
-describe("Test password", function() {
+describe("Test password", function () {
     const validPasswordsByLowerCase = ["diibadaaba", "Labrador", "0Abbasil", "Lubre", "yeah!", "Yeah!", "9a", "a9", "A9a", "a9A", "aFf3cted!", "F00baryMaru!"];
     const validPasswordsByUpperCase = ["Labrador", "0Abbasil", "L", "Lubre", "Yeah!", "A9", "9Z2", "A9a", "a9A", "aFf3cted!", "F00baryMaru!"];
-    const validPasswordsByPunctuation =["!", "-", "_", ".", "\"", "'", "yeah!", "Yeah!", "aFf3cted!", "F00baryMaru!"];
+    const validPasswordsByPunctuation = ["!", "-", "_", ".", "\"", "'", "yeah!", "Yeah!", "aFf3cted!", "F00baryMaru!"];
     const validPasswordsByDigit = ["0", "0Abbasil", "991239", "9a", "a9", "A9", "9Z2", "A9a", "a9A", "aFf3cted!", "F00baryMaru!"];
     const invalidPasswords = ["", " ", "   ", "\t\f\b\n", "\n"];
-    const validPasswords = validPasswordsByDigit.filter( 
+    const validPasswords = validPasswordsByDigit.filter(
         password => validPasswordsByUpperCase.includes(password)
     ).filter(
         password => validPasswordsByLowerCase.includes(password)
@@ -24,10 +24,10 @@ describe("Test password", function() {
         const regex = HAS_DIGIT_REGEX;
         const validSet = validPasswordsByDigit;
         const invalidFilter = (password) => (!validSet.includes(password));
-        validSet.forEach( tested => {
+        validSet.forEach(tested => {
             expect(regex.test(tested)).true;
         });
-        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach( tested => {
+        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach(tested => {
             expect(regex.test(tested)).false;
         });
     });
@@ -35,10 +35,10 @@ describe("Test password", function() {
         const regex = HAS_LOWER_CASE_LETTER_REGEX;
         const validSet = validPasswordsByLowerCase;
         const invalidFilter = (password) => (!validSet.includes(password));
-        validSet.forEach( tested => {
+        validSet.forEach(tested => {
             expect(regex.test(tested)).true;
         });
-        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach( tested => {
+        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach(tested => {
             expect(regex.test(tested)).false;
         });
     });
@@ -46,10 +46,10 @@ describe("Test password", function() {
         const regex = HAS_UPPER_CASE_LETTER_REGEX;
         const validSet = validPasswordsByUpperCase;
         const invalidFilter = (password) => (!validSet.includes(password));
-        validSet.forEach( tested => {
+        validSet.forEach(tested => {
             expect(regex.test(tested)).true;
         });
-        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach( tested => {
+        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach(tested => {
             expect(regex.test(tested)).false;
         });
     });
@@ -57,10 +57,10 @@ describe("Test password", function() {
         const regex = HAS_PUNCTUATION_REGEX;
         const validSet = validPasswordsByPunctuation;
         const invalidFilter = (password) => (!validSet.includes(password));
-        validSet.forEach( tested => {
+        validSet.forEach(tested => {
             expect(regex.test(tested)).true;
         });
-        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach( tested => {
+        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach(tested => {
             expect(regex.test(tested)).false;
         });
     });
@@ -68,23 +68,56 @@ describe("Test password", function() {
         const regex = HAS_DIGIT_REGEX;
         const validSet = validPasswordsByDigit;
         const invalidFilter = (password) => (!validSet.includes(password));
-        validSet.forEach( tested => {
+        validSet.forEach(tested => {
             expect(regex.test(tested)).true;
         });
-        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach( tested => {
+        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach(tested => {
             expect(regex.test(tested)).false;
         });
     });
-    it("Valid password regex", function() {
+    it("Valid password regex", function () {
         const regex = VALID_PASSWORD_REGEX;
         const validSet = validPasswords;
         const invalidFilter = (password) => (!validSet.includes(password));
-        validSet.forEach( tested => {
+        validSet.forEach(tested => {
             expect(regex.test(tested)).true;
         });
-        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach( tested => {
+        [...invalidPasswords, ...(validPasswords.filter(invalidFilter))].forEach(tested => {
             expect(regex.test(tested)).false;
         });
 
     })
+})
+
+describe("In Memory Api Service", function () {
+    it("Construction", function () {
+        [{ caseName: "Empty parameters", params: {} }, { caseName: "Default Parameters", params: undefined, exception: TypeError },
+        {
+            caseName: "With valid crypt options of simple sha1 of length 1", params: {
+                cryptOptions: {
+                    algorithm: "sha1",
+                    length: 1
+                }
+            }
+        }
+        ].forEach(({ caseName, params, exception = null }, caseIndex) => {
+            console.log(`Test case ${caseIndex}: name=${caseName} params=${params}, exception=${exception}`
+            );
+            const testFunc = () => {
+                new InMemoryApiService(params);
+            };
+            if (exception == null) {
+                it(`Test Case without error #${caseIndex}: ${caseName}`, function () {
+                    console.log("Without exception");
+                    expect(() => {testFunc()}).not.throw();
+                })
+            } else {
+                it(`Test Case with error #${caseIndex}: ${caseName}`, function () {
+
+                    console.log("With exception");
+                    expect(() => {testFunc()}).throw(exception);
+                });
+            }
+        });
+    });
 })
